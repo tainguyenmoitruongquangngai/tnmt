1b343572d2c4147206f7ee7aeb2f69a2635df197
tainguyenmoitruongsonla <tainguyenmoitruongsonla@gmail.com>
2024-02-11 09:56:03 +0700

    lan thu thay doi

--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -67,11 +67,11 @@ const navigation = (): VerticalNavItemsType => {
           path: '/giam-sat/quan-ly-yeu-cau-ket-noi'
         },
         {
-          title: 'KT,SD nước mặt',
+          title: 'KTSD nước mặt',
           path: '/giam-sat/nuoc-mat'
         },
         {
-          title: 'KT,SD nước dưới đất',
+          title: 'KTSD nước dưới đất',
           path: '/giam-sat/nuoc-duoi-dat'
         },
         {
