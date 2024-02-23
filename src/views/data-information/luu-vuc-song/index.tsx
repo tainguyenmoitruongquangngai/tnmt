4f18b37fceb295d0ebbf2c3cb4c0143b8523e0c6
tiramiSoo <89087554+trungquan21@users.noreply.github.com>
2024-02-23 16:57:05 +0700

    #TTDL_NguonNuoc_LVS

--- src/views/data-information/luu-vuc-song/index.tsx
+++ src/views/data-information/luu-vuc-song/index.tsx
@@ -1,84 +1,225 @@
-import { Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
+import Paper from '@mui/material/Paper'
+import { Grid, Typography, Box, } from '@mui/material'
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import Header from '../header'
+import Footer from '../footer'
+import { getData } from 'src/api/axios'
+import { useEffect, useState } from 'react'
+import BoxLoading from 'src/@core/components/box-loading'
+import dayjs from 'dayjs'
+import TableComponent, { TableColumn } from 'src/@core/components/table'
+import CreateNN_LuuVucSong from '../create-form/CreateNN_LuuVucSong'
+import DeleteData from 'src/@core/components/delete-data'
+import ToolBar from '../toolbar'
 
-const LuuVucSongPage = () => {
-   
-        return (
-          <Grid>
-            <Grid className='_text_center'>
-            <Typography className='font-weight-bold ' variant='h6'>
+const NN_LuuVucSong = () => {
+  const [data, setData] = useState<any[]>([])
+
+  const [loading, setLoading] = useState(false)
+  const [selectedYear, setSelectedYear] = useState<number>(new Date().getFullYear())
+
+  const [postSuccess, setPostSuccess] = useState(false);
+  const handlePostSuccess = () => {
+    setPostSuccess(prevState => !prevState);
+  };
+  useEffect(() => {
+    async function getDataNN_LuuVucSong() {
+      setLoading(true)
+      await getData('NN_LuuVucSong/danh-sach')
+        .then(data => {
+          setData(data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+        .finally(() => {
+          setLoading(false)
+        })
+    }
+
+    getDataNN_LuuVucSong()
+  }, [postSuccess])
+
+  const columnsTable: TableColumn[] = [
+    {
+      id: 'stt',
+      label: 'STT',
+      rowspan: 3
+    },
+    {
+      id: 'maSong',
+      label: 'Mã sông',
+      align: 'left',
+      rowspan: 2,
+      minWidth: 100,
+      children: [
+        {
+          id: '#1',
+          children: [{ id: '#1.1', label: '(1)', align: 'left' }]
+        }
+      ]
+    },
+    {
+      id: 'capSong',
+      label: 'Cấp sông',
+      align: 'left',
+      rowspan: 2,
+      children: [
+        {
+          id: '#2',
+          children: [{ id: '#2.1', label: '(2)', align: 'left' }]
+        }
+      ]
+    },
+    {
+      id: 'tenSongSuoi',
+      label: 'Tên sông, suối',
+      align: 'left',
+      rowspan: 2,
+      children: [
+        {
+          id: '#3',
+          children: [{ id: '#3.1', label: '(3)', align: 'left' }]
+        }
+      ]
+    },
+    {
+      id: 'chayRa',
+      label: 'Chảy ra',
+      align: 'left',
+      rowspan: 2,
+      children: [
+        {
+          id: '#4',
+          children: [{ id: '#4.1', label: '(4)', align: 'left' }]
+        }
+      ]
+    },
+    {
+      id: 'chieuDai',
+      label: 'Chiều dài (km)',
+      align: 'left',
+      rowspan: 2,
+      children: [
+        {
+          id: '#5',
+          children: [{ id: '#5.1', label: '(5)', align: 'left' }]
+        }
+      ]
+    },
+    {
+      id: 'dienTich',
+      label: 'Diện tích lưu vực (km2)',
+      align: 'left',
+      rowspan: 2,
+      children: [
+        {
+          id: '#6',
+          children: [{ id: '#6.1', label: '(6)', align: 'left' }]
+        }
+      ]
+    },
+    {
+      id: 'tinh',
+      label: 'Tỉnh/ Thành phố',
+      align: 'left',
+      rowspan: 2,
+      children: [
+        {
+          id: '#9',
+          children: [{ id: '#9.1', label: '(9)', align: 'left' }]
+        }
+      ]
+    },
+    {
+      id: 'thuocLVS',
+      label: 'Thuộc lưu vực sông',
+      align: 'left',
+      rowspan: 2,
+      children: [
+        {
+          id: '#10',
+          children: [{ id: '#10.1', label: '(10)', align: 'left' }]
+        }
+      ]
+    },
+    {
+      id: 'loaiSongSuoi',
+      label: 'Loại sông, suối',
+      align: 'left',
+      rowspan: 2,
+      children: [
+        {
+          id: '#10',
+          children: [{ id: '#10.1', label: '(10)', align: 'left' }]
+        }
+      ]
+    },
+    {
+      id: 'ghiChu',
+      label: 'Ghi chú',
+      align: 'left',
+      rowspan: 2,
+      children: [
+        {
+          id: '#10',
+          children: [{ id: '#10.1', label: '(10)', align: 'left' }]
+        }
+      ]
+    },
+    {align: 'center', id: 'actions', label: 'Thao tác', minWidth: 150, rowspan: 3 }
+  ]
+
+  return (
+    <Paper sx={{ p: 8 }}>
+      <Header />
+
+      <Grid className='_text_center'>
+        <Typography className='font-weight-bold ' variant='h6'>
           BÁO CÁO
         </Typography>
-              <Typography className='font-weight-bold' sx={{ mt: 3 }} variant='h6'>
-             THÔNG TIN DỮ LIỆU VỀ LƯU VỰC SÔNG TỈNH QUẢNG NGÃI
-              </Typography>
-            </Grid>
-      
-            <Grid className='_text_center' sx={{ mt: 3 }}>
-            <TableContainer component={Paper} sx={{ mt: 5 }}>
-              <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-                <TableHead className='tableHead'>
-                  <TableRow>
-                    <TableCell size='small' align='center' >
-                      STT
-                    </TableCell>
-                    <TableCell size='small' align='center' >
-                      Mã sông
-                    </TableCell>
-                    <TableCell size='small' align='center' >
-                      Cấp sông
-                    </TableCell>
-                    <TableCell size='small' align='center' >
-                      Tên sông, suối
-                    </TableCell>
-                    <TableCell size='small' align='center' >
-                    Chảy ra
-                    </TableCell>
-                    <TableCell size='small' align='center' >
-                   Chiều dài (km)
-                    </TableCell>
-                    <TableCell size='small' align='center'>
-                    Diện tích <br/> lưu vực <br/> (km2)
-                    </TableCell>
-                    <TableCell size='small' align='center'>
-                    Tỉnh / Thành phố
-                    </TableCell>
-                    <TableCell size='small' align='center'>
-                    Thuộc <br/> lưu vực sông
-                    </TableCell>
-                    <TableCell size='small' align='center'>
-                    Loại <br/> sông, suối 
-                    </TableCell>
-                    <TableCell size='small' align='center' >
-                      Ghi chú
-                    </TableCell>
-                    <TableCell size='small' align='center' >
-                      Thao tác
-                    </TableCell>
-                  </TableRow>
-      
-                </TableHead>
-      
-                <TableBody className='tableBody'>
-                  <TableRow>
-                    <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </TableContainer>
-            </Grid>
-          </Grid>
-        )
-      }
+        <Typography className='font-weight-bold ' variant='h6'>
+          THÔNG TIN DỮ LIỆU VỀ LƯU VỰC SÔNG TỈNH QUẢNG NGÃI
+        </Typography>
+        <Typography className='font-weight-bold ' variant='h6'>
+          (Kỳ báo cáo:{' '}
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              views={['year']}
+              value={dayjs(new Date(selectedYear, 1, 1))}
+              onChange={(newVal: any) => setSelectedYear(newVal.year())}
+              slotProps={{ textField: { size: 'small', fullWidth: true, required: true } }}
+              sx={{ width: '100px' }}
+            />
+          </LocalizationProvider>
+          )
+        </Typography>
+      </Grid>
+      {/* <CreateReport2 isEdit={false} setPostSuccess={handlePostSuccess}/> */}
+      {loading ? (
+        <BoxLoading />
+      ) : (
+        <Grid className='_text_center' sx={{ mt: 3 }}>
+          <ToolBar onExport={{ data: data, column: columnsTable }} />
+          <TableComponent
+            columns={columnsTable}
+            rows={data}
+            loading={loading}
+            pagination
+            actions={(row: any) => (
+              <Box >
+                <CreateNN_LuuVucSong isEdit={true} data={row} setPostSuccess={handlePostSuccess} />
+                <DeleteData url={'NN_LuuVucSong'} data={row} setPostSuccess={handlePostSuccess} />
+              </Box>
+            )}
+          />
+        </Grid>
+      )}
 
-export default LuuVucSongPage
+      <Footer />
+    </Paper>
+  )
+}
+
+export default NN_LuuVucSong
