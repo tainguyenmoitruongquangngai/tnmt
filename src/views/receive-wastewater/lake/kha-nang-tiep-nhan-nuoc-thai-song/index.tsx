20bd5cf1dd329c61a5c910d9d6d588233e79eb07
trungquan21 <89087554+trungquan21@users.noreply.github.com>
2024-03-19 18:45:25 +0700

    #khanangtiepnhannguonthai

--- src/views/receive-wastewater/lake/kha-nang-tiep-nhan-nuoc-thai-song/index.tsx
+++ src/views/receive-wastewater/lake/kha-nang-tiep-nhan-nuoc-thai-song/index.tsx
@@ -1,5 +1,5 @@
 //React Imports
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 
 //MUI Imports
@@ -7,22 +7,164 @@ import { useState } from 'react'
 import Grid from '@mui/material/Unstable_Grid2'
 
 //import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic'
+import { getData } from 'src/api/axios'
+import {  Paper, Typography } from '@mui/material'
+import TableComponent, { TableColumn } from 'src/@core/components/table'
 
-import dynamic from 'next/dynamic';
-import KhaNangTiepNhanNuocThaiSong1 from './wasteWaterReceiTableSong';
-
-const Map = dynamic(() => import("src/@core/components/map"), { ssr: false });
-
-//const Map = dynamic(() => import('src/@core/components/map'), { ssr: false })
+const Map = dynamic(() => import('src/@core/components/map'), { ssr: false })
 
 // eslint-disable-next-line react-hooks/rules-of-hooks
 const KhaNangTiepNhanNuocThaiSong = () => {
   //Init columnTable
 
-  const [mapCenter] = useState([ 15.012172, 108.676488 ]);
-  const [mapZoom] = useState(9);
-
+  // const [mapCenter, setMapCenter] = useState([15.012172, 108.676488])
+  // const [mapZoom, setMapZoom] = useState(9)
   // const [showLabel, setShowLabel] = useState(false)
+  const columnsTable: TableColumn[] = [
+    { id: 'stt', label: 'STT', rowspan: 2 },
+    { id: 'luuVucSong', label: 'Lưu vực sông', rowspan: 2, align: 'left', minWidth: 200},
+
+    { id: 'song', label: 'Sông', rowspan: 2, align: 'left', minWidth: 200},
+    {
+      id: 'tenDoanSong',
+      label: 'Tên đoạn sông',
+      rowspan: 2,
+      align: 'left',
+      minWidth: 100
+    },
+    {
+      id: 'phanDoan',
+      label: 'Phân đoạn sông',
+      rowspan: 2,
+      align: 'left',
+      minWidth: 200
+    },
+    {
+      id: 'chieuDai',
+      label: (
+        <>
+          Chiều dài <br /> đoạn sông <br /> (km)
+        </>
+      ),
+      rowspan: 2,
+      align: 'left',
+      minWidth: 100,
+    },
+    
+    {
+      id: '#',
+      label: (
+        <>
+          KHẢ NĂNG TIẾP NHẬN NƯỚC THẢI, SỨC CHỊU TẢI<br />
+          Ltd (kg/ngày)
+        </>
+      ),
+      align: 'left',
+      children: [
+        {
+          id: 'ltnBod',
+          label: (
+            <>
+              BOD5 <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltnCod',
+          label: (
+            <>
+              COD <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltnAmoni',
+          label: (
+            <>
+              Amoni <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltnTongN',
+          label: (
+            <>
+              Tổng N <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltnTongP',
+          label: (
+            <>
+              Tổng P <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltnTSS',
+          label: (
+            <>
+              Tổng <br /> chất rắn <br /> lơ lửng <br /> TSS(mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltnColiform',
+          label: (
+            <>
+              Tổng P <br /> coliform
+              <br /> (MPN/100ml)
+            </>
+          ),
+          align: 'left'
+        }
+      ]
+    },
+    {
+      id: 'ghiChu',
+      label: 'Ghi chú',
+      rowspan: 2,
+      align: 'left'
+    },
+  ]
+
+  const [mapCenter] = useState([15.012172, 108.676488])
+  const [mapZoom] = useState(9)
+  const [data, setData] = useState([])
+  console.log(data)
+
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    async function getDataReport1() {
+      setLoading(true)
+      await getData('PhanDoanSong/tai-luong')
+        .then(data => {
+          setData(data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+        .finally(() => {
+          setLoading(false)
+        })
+    }
+
+    getDataReport1()
+  }, [])
 
   // const zoomConstruction = (coords: any) => {
   //   setMapCenter(coords)
@@ -47,13 +189,24 @@ const KhaNangTiepNhanNuocThaiSong = () => {
           </Box>
           <Map center={mapCenter} zoom={mapZoom} showLabel={showLabel} loading={false} />
         </Paper> */}
-        <Map center={mapCenter} zoom={mapZoom} mapData={null} />
+        <Map center={mapCenter} zoom={mapZoom} loading={false} />
       </Grid>
       <Grid xs={12} md={12}>
-       <KhaNangTiepNhanNuocThaiSong1/>
+      <Grid className='_text_center'>
+          <Typography className='font-weight-bold' sx={{ mt: 3 }} variant='h6'>
+          KHẢ NĂNG TIẾP NHẬN NƯỚC THẢI, SỨC CHỊU TẢI CỦA NGUỒN NƯỚC SÔNG, SUỐI TRÊN ĐỊA BÀN TỈNH QUẢNG NGÃI
+          </Typography>
+        </Grid>
+        <Paper elevation={3} sx={{ p: 0, height: '100%' }}>
+          <TableComponent
+            columns={columnsTable}
+            rows={data}
+            loading={loading}
+            pagination
+          />
+        </Paper>
       </Grid>
     </Grid>
-    
   )
 }
 
--- src/views/receive-wastewater/lake/tai-luong-o-nhiem/index.tsx
+++ src/views/receive-wastewater/lake/tai-luong-o-nhiem/index.tsx
@@ -1,5 +1,5 @@
 //React Imports
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 
 //MUI Imports
@@ -7,23 +7,336 @@ import { useState } from 'react'
 import Grid from '@mui/material/Unstable_Grid2'
 
 //import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic'
+import { getData } from 'src/api/axios'
+import {  Paper } from '@mui/material'
+import TableComponent, { TableColumn } from 'src/@core/components/table'
 
-
-import TaiLuongONhiem from './wasteWaterReceiTable'
-import dynamic from 'next/dynamic';
-
-const Map = dynamic(() => import("src/@core/components/map"), { ssr: false });
-
-//const Map = dynamic(() => import('src/@core/components/map'), { ssr: false })
+const Map = dynamic(() => import('src/@core/components/map'), { ssr: false })
 
 // eslint-disable-next-line react-hooks/rules-of-hooks
-const WasteWaterReceDetails = () => {
+const TaiLuongONhiem = () => {
   //Init columnTable
 
-  const [mapCenter] = useState([ 15.012172, 108.676488 ]);
-  const [mapZoom] = useState(9);
-
+  // const [mapCenter, setMapCenter] = useState([15.012172, 108.676488])
+  // const [mapZoom, setMapZoom] = useState(9)
   // const [showLabel, setShowLabel] = useState(false)
+  const columnsTable: TableColumn[] = [
+    { id: 'stt', label: 'STT', rowspan: 2 },
+    { id: 'luuVucSong', label: 'Lưu vực sông', rowspan: 2, align: 'left', minWidth: 200},
+
+    { id: 'song', label: 'Sông', rowspan: 2, align: 'left', minWidth: 200},
+    {
+      id: 'tenDoanSong',
+      label: 'Tên đoạn sông',
+      rowspan: 2,
+      align: 'left',
+      minWidth: 100
+    },
+    {
+      id: 'phanDoan',
+      label: 'Phân đoạn sông',
+      rowspan: 2,
+      align: 'left',
+      minWidth: 200
+    },
+    {
+      id: 'chieuDai',
+      label: (
+        <>
+          Chiều dài <br /> đoạn sông <br /> (km)
+        </>
+      ),
+      rowspan: 2,
+      align: 'left',
+      minWidth: 100,
+    },
+    
+    {
+      id: 'duLieuNguonNuocNhan',
+      label: (
+        <>
+          TẢI LƯỢNG TỐI ĐA CỦA THÔNG SỐ CHẤT LƯỢNG NƯỚC MẶT<br />
+          Ltd (kg/ngày)
+        </>
+      ),
+      align: 'left',
+      children: [
+        {
+          id: 'ltdBOD',
+          label: (
+            <>
+              BOD5 <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltdCOD',
+          label: (
+            <>
+              COD <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltdAmoni',
+          label: (
+            <>
+              Amoni <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltdTongN',
+          label: (
+            <>
+              Tổng N <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltdTongP',
+          label: (
+            <>
+              Tổng P <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltdTSS',
+          label: (
+            <>
+              Tổng <br /> chất rắn <br /> lơ lửng <br /> TSS(mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltdColiform',
+          label: (
+            <>
+              Tổng P <br /> coliform
+              <br /> (MPN/100ml)
+            </>
+          ),
+          align: 'left'
+        }
+      ]
+    },
+
+    //lnn
+    {
+      id: 'duLieuNguonNuocNhan',
+      label: (
+        <>
+          {' '}
+          TẢI LƯỢNG Ô NHIỄM NGUỒN NƯỚC HIỆN CÓ <br />
+          Lnn (kg/ngày)
+        </>
+      ),
+      align: 'left',
+      children: [
+        {
+          id: 'lnnBOD',
+          label: (
+            <>
+              BOD5 <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'lnnCOD',
+          label: (
+            <>
+              COD <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'lnnAmoni',
+          label: (
+            <>
+              Amoni <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'lnnTongN',
+          label: (
+            <>
+              Tổng N <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'lnnTongP',
+          label: (
+            <>
+              Tổng P <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'lnnTSS',
+          label: (
+            <>
+              Tổng <br /> chất rắn <br /> lơ lửng <br /> TSS(mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'lnnColiform',
+          label: (
+            <>
+              Tổng P <br /> coliform
+              <br /> (MPN/100ml)
+            </>
+          ),
+          align: 'left'
+        }
+      ]
+    },
+
+    {
+      id: '#',
+      label: (
+        <>
+          TỔNG TẢI LƯỢNG Ô NHIỄM CỦA NGUỒN NƯỚC THẢI <br />
+          Lt (kg/ngày)
+        </>
+      ),
+      align: 'left',
+      children: [
+        {
+          id: 'ltBod',
+          label: (
+            <>
+              BOD5 <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltCod',
+          label: (
+            <>
+              COD <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltAmoni',
+          label: (
+            <>
+              Amoni <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltTongN',
+          label: (
+            <>
+              Tổng N <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltTongP',
+          label: (
+            <>
+              Tổng P <br />
+              (mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltTSS',
+          label: (
+            <>
+              Tổng <br /> chất rắn <br /> lơ lửng <br /> TSS(mg/l)
+            </>
+          ),
+          align: 'left'
+        },
+        {
+          id: 'ltColiform',
+          label: (
+            <>
+              Tổng P <br /> coliform
+              <br /> (MPN/100ml)
+            </>
+          ),
+          align: 'left'
+        }
+      ]
+    },
+    {
+      id: '#',
+      label: 'Hệ số an toàn ',
+      rowspan: 2,
+      align: 'left'
+    },
+    {
+      id: 'ghiChu',
+      label: 'Ghi chú',
+      rowspan: 2,
+      align: 'left'
+    },
+  ]
+
+  const [mapCenter] = useState([15.012172, 108.676488])
+  const [mapZoom] = useState(9)
+  const [data, setData] = useState([])
+  console.log(data)
+
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    async function getDataReport1() {
+      setLoading(true)
+      await getData('PhanDoanSong/tai-luong')
+        .then(data => {
+          setData(data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+        .finally(() => {
+          setLoading(false)
+        })
+    }
+
+    getDataReport1()
+  }, [])
 
   // const zoomConstruction = (coords: any) => {
   //   setMapCenter(coords)
@@ -48,14 +361,21 @@ const WasteWaterReceDetails = () => {
           </Box>
           <Map center={mapCenter} zoom={mapZoom} showLabel={showLabel} loading={false} />
         </Paper> */}
-        <Map center={mapCenter} zoom={mapZoom} mapData={null} />
+        <Map center={mapCenter} zoom={mapZoom} loading={false} />
       </Grid>
       <Grid xs={12} md={12}>
-       <TaiLuongONhiem/>
+        <Paper elevation={3} sx={{ p: 0, height: '100%' }}>
+        
+          <TableComponent
+            columns={columnsTable}
+            rows={data}
+            loading={loading}
+            pagination
+          />
+        </Paper>
       </Grid>
     </Grid>
-    
   )
 }
 
-export default WasteWaterReceDetails
+export default TaiLuongONhiem
