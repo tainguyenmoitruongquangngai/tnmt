d5ec43cb2197e7577700ff77a498b7851d5916fd
trungquan21 <89087554+trungquan21@users.noreply.github.com>
2024-04-03 15:53:48 +0700

    #update phan doan song

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -6,31 +6,38 @@ const Test = () => {
     const column: TableColumn[] = [
         {
             id: '#', label: 'column1', children: [
-                { id: '#', label: 'column1.1', rowspan: 3 },
-                { id: '#', label: 'column1.2', rowspan: 3 },
-                { id: '#', label: 'column1.3', rowspan: 3 }
+                { id: '#', label: 'column1.1',children: [
+                    { id: '#', label: 'column2.3.1',children: [
+                        { id: '#', label: 'column2.3.1',  },
+                        { id: '#', label: 'column2.3.2',  }
+                    ]  },
+                    { id: '#', label: 'column2.3.2',children: [
+                        { id: '#', label: 'column2.3.1',  },
+                        { id: '#', label: 'column2.3.2',  }
+                    ]  }
+                ]},
             ]
         },
         {
             id: '#', label: 'column2', children: [
                 {
                     id: '#', label: 'column2.1', children: [
-                        { id: '#', label: 'column2.1.1', rowspan: 2 },
-                        { id: '#', label: 'column2.1.2', rowspan: 2 },
-                        { id: '#', label: 'column2.1.3', rowspan: 2 }
+                        { id: '#', label: 'column2.1.1',  },
+                        { id: '#', label: 'column2.1.2',  },
+                        { id: '#', label: 'column2.1.3',  }
                     ]
                 },
                 {
                     id: '#', label: 'column2.2', children: [
-                        { id: '#', label: 'column2.2.1', rowspan: 2 },
-                        { id: '#', label: 'column2.2.2', rowspan: 2 },
-                        { id: '#', label: 'column2.2.3', rowspan: 2 }
+                        { id: '#', label: 'column2.2.1',  },
+                        { id: '#', label: 'column2.2.2',  },
+                        { id: '#', label: 'column2.2.3',  }
                     ]
                 },
                 {
                     id: '#', label: 'column2.3', children: [
-                        { id: '#', label: 'column2.3.1', rowspan: 2 },
-                        { id: '#', label: 'column2.3.2', rowspan: 2 }
+                        { id: '#', label: 'column2.3.1',  },
+                        { id: '#', label: 'column2.3.2',  }
                     ]
                 }
             ]
@@ -52,13 +59,13 @@ const Test = () => {
                                 { id: '#', label: 'column3.3.1.3' }
                             ]
                         },
-                        { id: '#', label: 'column3.1.2', rowspan: 3 }
+                        { id: '#', label: 'column3.1.2',}
                     ]
                 },
                 {
                     id: '#', label: 'column3.2', children: [
-                        { id: '#', label: 'column3.2.1', rowspan: 2 },
-                        { id: '#', label: 'column3.2.2', rowspan: 2 },
+                        { id: '#', label: 'column3.2.1',  },
+                        { id: '#', label: 'column3.2.2',  },
                     ]
                 },
             ]
--- src/views/receive-wastewater/lake/phan-doan-song/index.tsx
+++ src/views/receive-wastewater/lake/phan-doan-song/index.tsx
@@ -6,16 +6,12 @@ import { useState } from 'react'
 //import { Box, Paper, FormGroup, FormControlLabel, Checkbox } from '@mui/material'
 import Grid from '@mui/material/Unstable_Grid2'
 
-//import dynamic from 'next/dynamic'
-import dynamic from 'next/dynamic'
 import { getData } from 'src/api/axios'
-import { PhanDoanSongState } from './PhanDoanSongInterface'
-import BoxLoading from 'src/@core/components/box-loading'
-import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import { Box, Paper, Typography } from '@mui/material'
+import TableComponent, { TableColumn } from 'src/@core/components/table'
 import DeleteData from 'src/@core/components/delete-data'
 import PhanDoanSongForm from './PhanDoanSongForm'
-
-const Map = dynamic(() => import('src/@core/components/map'), { ssr: false })
+import ExportToExcel from 'src/@core/components/export-excel'
 
 // eslint-disable-next-line react-hooks/rules-of-hooks
 const PhanDoanSongTiepNhanNuocThai = () => {
@@ -24,12 +20,88 @@ const PhanDoanSongTiepNhanNuocThai = () => {
   // const [mapCenter, setMapCenter] = useState([15.012172, 108.676488])
   // const [mapZoom, setMapZoom] = useState(9)
   // const [showLabel, setShowLabel] = useState(false)
+  const columnsTable: TableColumn[] = [
+    { id: 'stt', label: 'STT' },
+    {
+      id: 'luuVucSong',
+      label: 'Lưu vực sông',
+      align: 'left',
+      minWidth: 200
+    },
+    {
+      id: 'song',
+      label: 'Sông',
+      align: 'left',
+      minWidth: 200
+    },
+    {
+      id: 'tenDoanSong',
+      label: 'Tên đoạn sông',
+      align: 'left',
+      minWidth: 200
+    },
+    {
+      id: 'phanDoan',
+      label: 'Phân đoạn sông',
+      align: 'left',
+      minWidth: 200
+    },
+    {
+      id: '#',
+      label: 'Tọa độ (VN2000, múi chiếu 30, kinh tuyến trục 107045’)',
+      children: [
+        {
+          id: 'xDau',
+          label: 'Điểm đầu X'
+        },
+        {
+          id: 'yDau',
+          label: 'Điểm đầu Y'
+        },
+        {
+          id: 'xCuoi',
+          label: 'Điểm cuối X'
+        },
+        {
+          id: 'xCuoi',
+          label: 'Điểm cuối Y'
+        }
+      ]
+    },
+    {
+      id: 'diaGioiHanhChinh',
+      label: 'Địa giới hành chính',
+      align: 'left'
+    },
+    {
+      id: 'mucDichSuDung',
+      label: (
+        <>
+          Mục đích sử dụng nước <br /> theo QCVN 08:2023/BTNMT
+        </>
+      ),
+      align: 'left'
+    },
+    {
+      id: 'chatLuongNuoc',
+      label: (
+        <>
+          Chất lượng nước <br /> theo QCVN 08:2023 ứng
+        </>
+      ),
+      align: 'left'
+    },
 
-  const [mapCenter] = useState([15.012172, 108.676488])
-  const [mapZoom] = useState(9)
-  const [data, setData] = useState<PhanDoanSongState[]>([])
-  console.log(data)
+    {
+      id: 'ghiChu',
+      label: 'Ghi chú',
+      align: 'left'
+    },
 
+    { id: 'actions', label: 'Thao tác', align: 'center', pinned: 'right' }
+  ]
+
+  const [data, setData] = useState([])
   const [loading, setLoading] = useState(false)
   const [postSuccess, setPostSuccess] = useState(false)
   const handlePostSuccess = () => {
@@ -64,141 +136,37 @@ const PhanDoanSongTiepNhanNuocThai = () => {
 
   return (
     <Grid container spacing={2}>
-      <Grid xs={12} md={12} sx={{ height: '55vh', overflow: 'hidden' }}>
-        {/* <Paper elevation={3} sx={{ height: '100%', position: 'relative' }}>
-          <Box className='map-legend' sx={{ background: 'white', pl: 2 }}>
-            <FormGroup>
-              <FormControlLabel
-                control={<Checkbox onClick={() => setShowLabel(!showLabel)} />}
-                label='Hiển thị tên công trình'
-              />
-            </FormGroup>
-            <MapLegend onChange={handleConsTypeChange} />
-          </Box>
-          <Map center={mapCenter} zoom={mapZoom} showLabel={showLabel} loading={false} />
-        </Paper> */}
-        <Map center={mapCenter} zoom={mapZoom} loading={false} />
-      </Grid>
-      <Grid className='_flexEnd'>
-        <PhanDoanSongForm isEdit={false} setPostSuccess={handlePostSuccess} />
-      </Grid>
-
-      {loading ? (
-        <BoxLoading />
-      ) : (
-        <Grid className='_text_center' sx={{ mt: 3 }}>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-              <TableHead className='tableHead'>
-                <TableRow>
-                  <TableCell size='small' align='center' rowSpan={3}>
-                    STT
-                  </TableCell>
-                  <TableCell size='small' align='center' rowSpan={3}>
-                    Lưu vực sông
-                  </TableCell>
-                  <TableCell size='small' align='center' rowSpan={3}>
-                    Sông
-                  </TableCell>
-                  <TableCell size='small' align='center' rowSpan={3}>
-                    Tên đoạn sông
-                  </TableCell>
-                  <TableCell size='small' align='center' rowSpan={3}>
-                    Phân đoạn sông
-                  </TableCell>
-                  <TableCell size='small' align='center' rowSpan={3}>
-                    Chiều dài <br /> đoạn sông (km)
-                  </TableCell>
-                  <TableCell size='small' align='center' rowSpan={3}>
-                    Diện tích <br /> lưu vực (km2)
-                  </TableCell>
-
-                  <TableCell size='small' align='center' colSpan={4}>
-                    Tọa độ (VN2000, múi chiếu 30, kinh tuyến trục 107045’)
-                  </TableCell>
-
-                  <TableCell size='small' align='center' rowSpan={3} colSpan={1}>
-                    Địa giới hành chính
-                  </TableCell>
-
-                  <TableCell size='small' align='center' rowSpan={3} colSpan={1}>
-                    Mục đích sử dụng nước <br /> theo QCVN 08: 2023/ BTNMT
-                  </TableCell>
-                  <TableCell size='small' align='center' rowSpan={3}>
-                    Chất lượng nước <br /> theo QC 08/2023 ứng với <br /> mục đích sử dụng nước
-                  </TableCell>
-
-                  <TableCell size='small' align='center' rowSpan={3}>
-                    Ghi chú
-                  </TableCell>
-
-                  <TableCell size='small' align='center' rowSpan={3}>
-                    Thao tác
-                  </TableCell>
-                </TableRow>
-
-                <TableRow>
-                  <TableCell size='small' align='center' colSpan={2}>
-                    Điểm đầu
-                  </TableCell>
-                  <TableCell size='small' align='center' colSpan={2}>
-                    Điểm cuối
-                  </TableCell>
-                </TableRow>
-
-                <TableRow>
-                  <TableCell size='small' align='center'>
-                    X
-                  </TableCell>
-                  <TableCell size='small' align='center'>
-                    Y
-                  </TableCell>
-                  <TableCell size='small' align='center'>
-                    X
-                  </TableCell>
-                  <TableCell size='small' align='center'>
-                    Y
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-
-              <TableBody className='tableBody'>
-                {data.map((item, index) => (
-                  <TableRow key={item.id}>
-                    <TableCell className="text-center  size='small' align-middle font-13">{index + 1}</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">{item.luuVucSong}</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">{item.song}</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">{item.tenDoanSong}</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">{item.phanDoan}</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">{item.chieuDai}</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">{item.dienTichLV}</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">{item.xDau}</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">{item.yDau}</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">{item.xCuoi}</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">{item.yCuoi}</TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">
-                      {item.diaGioiHanhChinh}
-                    </TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">
-                      {item.mucDichSuDung}
-                    </TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">
-                      {item.chatLuongNuoc}
-                    </TableCell>
-                    <TableCell className="text-center  size='small' align-middle font-13">{item.ghiChu}</TableCell>
-                    <TableCell align='center' className="  size='small' align-middle font-13">
-                      <Box>
-                        <PhanDoanSongForm isEdit={true} data={item} setPostSuccess={handlePostSuccess} />
-                        <DeleteData url={'phan-doan-song'} data={item} setPostSuccess={handlePostSuccess} />
-                      </Box>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+      <Grid xs={12} md={12}>
+        <Grid className='_text_center'>
+          <Typography className='font-weight-bold' sx={{ mt: 3 }} variant='h6'>
+            THỐNG KÊ TẢI LƯỢNG CHẤT Ô NHIỄM TỪ NGUỒN THẢI ĐIỂM XẢ VÀO ĐOẠN SÔNG SUỐI TỈNH QUẢNG NGÃI
+          </Typography>
         </Grid>
-      )}
+        <Paper elevation={3} sx={{ p: 0, height: '100%' }}>
+          <Grid container className='_flexEnd' spacing={2} sx={{p:2}}>
+            <Grid >
+              <ExportToExcel resData={data} columnsTable={columnsTable} />
+            </Grid>
+
+            <Grid >
+            <PhanDoanSongForm isEdit={false} setPostSuccess={handlePostSuccess} />
+          </Grid>
+          </Grid>
+          
+          <TableComponent
+            columns={columnsTable}
+            rows={data}
+            loading={loading}
+            pagination
+            actions={(row: any) => (
+              <Box display={'flex'}>
+                <PhanDoanSongForm isEdit={true} data={row} setPostSuccess={handlePostSuccess} />
+                <DeleteData url={'du-lieu-nguon-nhan'} data={row} setPostSuccess={handlePostSuccess} />
+              </Box>
+            )}
+          />
+        </Paper>
+      </Grid>
     </Grid>
   )
 }
