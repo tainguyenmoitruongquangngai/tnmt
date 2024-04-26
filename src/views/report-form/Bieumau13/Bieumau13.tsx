28365a722884c355c322f2948517ec2abc19ac0e
NT.Dang.20 <73978541+githubnguyendang@users.noreply.github.com>
2024-04-26 18:15:34 +0700

    UpdateBieuMau13

--- src/views/report-form/Bieumau13/Bieumau13.tsx
+++ src/views/report-form/Bieumau13/Bieumau13.tsx
@@ -1,34 +1,26 @@
 import Paper from '@mui/material/Paper'
-import {
-  Grid,
-  TextField,
-  Typography,
-  Link,
-  Box,
-  IconButton,
-} from '@mui/material'
-import DownloadIcon from '@mui/icons-material/Download'
-import DialogControlFullScreen from 'src/@core/components/dialog-control-full-screen'
-import HeaderReport from '../HeaderReport'
-import FooterReport from '../FooterReport'
-import { getData } from 'src/api/axios'
-import { useEffect, useState } from 'react'
-import BoxLoading from 'src/@core/components/box-loading'
-import { Report13State } from './Report13InterFace'
-import CreateReport13 from './CreateReport13'
-import Report13Table from './Report13Table'
+import { Grid, Typography, Link, Box, } from '@mui/material';
+import DialogControlFullScreen from 'src/@core/components/dialog-control-full-screen';
+import HeaderReport from '../HeaderReport';
+import FooterReport from '../FooterReport';
+import { getData } from 'src/api/axios';
+import { useEffect, useState } from 'react';
+import BoxLoading from 'src/@core/components/box-loading';
+import TableComponent, { TableColumn } from 'src/@core/components/table';
+import ExportTableToExcel from 'src/@core/components/export-excel/export-csv';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
 
 const FormContruction = () => {
-  const [data, setData] = useState<Report13State[]>([])
+  const [data, setData] = useState<any[]>([])
   const [loading, setLoading] = useState(false)
-  const [postSuccess, setPostSuccess] = useState(false)
-  const handlePostSuccess = () => {
-    setPostSuccess(prevState => !prevState)
-  }
+  const [year, setYear] = useState(new Date().getFullYear() - 1)
+
   useEffect(() => {
     async function getDataReport1() {
       setLoading(true)
-      await getData('BieuMauSoMuoiBa/danhsach')
+      await getData('BaoCaoBieuMau/so13', { nam: year })
         .then(data => {
           setData(data)
         })
@@ -41,23 +33,100 @@ const FormContruction = () => {
     }
 
     getDataReport1()
-  }, [postSuccess])
+  }, [year])
+
+  const columnsTable: TableColumn[] = [
+    {
+      id: 'stt',
+      label: 'STT',
+    },
+    {
+      id: 'luuVucSong',
+      label: <>Lưu vực sông <br /> Vùng/Tỉnh</>,
+      align: 'left',
+    },
+    {
+      id: 'songSuoiHoChua',
+      label: <>Sông, suối <br /> hồ chứa</>,
+      align: 'left',
+    },
+    {
+      id: 'viTriQuanTrac',
+      label: <>Vị trí <br /> quan trắc</>,
+      align: 'left',
+      minWidth: 200,
+    },
+    {
+      id: '#',
+      label: <>Kết quả phân tích <br /> chỉ tiêu BOD5 [-]</>,
+      align: 'center',
+      children: [
+        {
+          id: 'boD5LonNhat',
+          label: 'Lớn nhất',
+          align: 'center',
+        },
+        {
+          id: 'boD5NhoNhat',
+          label: 'Nhỏ nhất',
+          align: 'center',
+        },
+        {
+          id: 'boD5TrungBinh',
+          label: 'Trung bình',
+          align: 'center',
+        },
+      ]
+    },
+    {
+      id: '#',
+      label: <>Kết quả phân tích <br /> chỉ tiêu COD [-]</>,
+      align: 'center',
+      children: [
+        {
+          id: 'codLonNhat',
+          label: 'Lớn nhất',
+          align: 'center',
+        },
+        {
+          id: 'codNhoNhat',
+          label: 'Nhỏ nhất',
+          align: 'center',
+        },
+        {
+          id: 'codTrungBinh',
+          label: 'Trung bình',
+          align: 'center',
+        },
+      ]
+    },
+    {
+      id: '#',
+      label: <>Kết quả phân tích <br /> chỉ tiêu DO [-]</>,
+      align: 'center',
+      children: [
+        {
+          id: 'doLonNhat',
+          label: 'Lớn nhất',
+          align: 'center',
+        },
+        {
+          id: 'doNhoNhat',
+          label: 'Nhỏ nhất',
+          align: 'center',
+        },
+        {
+          id: 'doTrungBinh',
+          label: 'Trung bình',
+          align: 'center',
+        },
+      ]
+    },
+  ]
 
   return (
+
     <Paper sx={{ p: 8 }}>
-      {/* dautrang */}
-      <Grid container>
-        <Grid md={11}>
-          <Typography variant='h5'>
-            Biểu mẫu số 13.Tổng hợp các đặc trưng của các chỉ tiêu cơ bản về chất lượng nước mặt
-          </Typography>
-        </Grid>
-        <Grid md={1}>
-          <IconButton>
-            <DownloadIcon />
-          </IconButton>
-        </Grid>
-      </Grid>
       <HeaderReport />
 
       <Grid className='_text_center'>
@@ -65,22 +134,42 @@ const FormContruction = () => {
           BÁO CÁO
         </Typography>
         <Typography className='font-weight-bold ' variant='h6'>
-          Tổng hợp các đặc trưng của các chỉ tiêu cơ bản về chất lượng nước mặt
+          Số lượng công trình khai thác nước mặt, nước dưới đất phân theo mục đích sử dụng
         </Typography>
         <Typography className='font-weight-bold ' variant='h6'>
-          (Kỳ báo cáo: <TextField size='small' sx={{ width: '50px' }}></TextField>)
+          (Kỳ báo cáo:
+          <Box sx={{ width: 100, ml: 2, display: 'inline-block' }}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label='Từ năm'
+                views={["year"]}
+                value={dayjs(new Date(year, 1, 1))}
+                onChange={(newVal: any) => setYear(dayjs(newVal).year())}
+                slotProps={{ textField: { size: 'small', required: true } }}
+              />
+            </LocalizationProvider>
+          </Box>
+          )
         </Typography>
       </Grid>
-      <CreateReport13 isEdit={false} setPostSuccess={handlePostSuccess} />
+
       {loading ? (
         <BoxLoading />
       ) : (
         <Grid className='_text_center' sx={{ mt: 3 }}>
-         <Report13Table data={data} setPostSuccess={handlePostSuccess}/>
+          <Box sx={{ width: 'max-content' }}><ExportTableToExcel tableId={'bieumau13'} filename={'bieumau13'} /></Box>
+          <TableComponent
+            columns={columnsTable}
+            id='bieumau13'
+            rows={data}
+            loading={loading}
+            actions={() => (
+              <Box >
+              </Box>
+            )}
+          />
         </Grid>
       )}
-
-
       <FooterReport />
     </Paper>
   )
@@ -96,7 +185,7 @@ const Bieumau13 = () => {
           <Link className='formReport_box' onClick={() => openDialogs(<FormContruction />, formTitle)}>
             <Grid item xs={8}>
               <Typography className='text-danger text-weight-bold'>Biểu mẫu 13</Typography>
-              <Typography className='text-success text-weight-bold _font12'>Chất lượng nước mặt</Typography>
+              <Typography className='text-success text-weight-bold _font12'>Số lượng CTKT phân theo sử dụng</Typography>
             </Grid>
             <Grid item xs={4}>
               <Box component='img' src='/images/report-form/ANHBIEUMAU13.png' className='formReport_img' alt='' />
