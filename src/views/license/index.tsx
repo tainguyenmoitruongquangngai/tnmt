36c428984f638b27bd9dbc8035e55189e7af91d1
NT.Dang.20 <73978541+githubnguyendang@users.noreply.github.com>
2024-05-16 18:15:38 +0700

    Update index.tsx

--- src/views/license/index.tsx
+++ src/views/license/index.tsx
@@ -79,7 +79,7 @@ const ListLicenses = () => {
                     id: "mucdich_kt", label: "Lưu lượng cấp phép", align: 'left', children: [
                         { id: "mucDich", label: "Mục đích", align: 'left', minWidth: 350, elm: (row: any) => (row.congtrinh?.mucdich_kt?.map((e: any, key: number, arr: any[]) => (<>{e.mucDich}{key < arr.length - 1 ? ', ' : ''}</>))) },
                         { id: "luuLuong", label: "Lưu lượng", align: 'center', minWidth: 150, elm: (row: any) => (row.congtrinh?.tongLuuLuong == 0 ? null : row.congtrinh?.tongLuuLuong) },
-                        { id: "donViTinhLuuLuong", label: "Đơn vị đo", align: 'center', minWidth: 150, elm: (row: any) => (row.congtrinh?.tongLuuLuong == 0 ? null : row.congtrinh?.donViTinhLuuLuong) },
+                        { id: "donViTinhLuuLuong", label: "Đơn vị đo", align: 'center', minWidth: 150, elm: (row: any) => (row.congtrinh?.donViTinhLuuLuong) },
                     ]
                 }
             ]
