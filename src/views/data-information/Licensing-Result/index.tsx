6086d856475336c1d2ea95c0971eac3888acc24b
NT.Dang.20 <73978541+githubnguyendang@users.noreply.github.com>
2024-05-21 15:14:44 +0700

    Update page license/license result

--- src/views/data-information/Licensing-Result/index.tsx
+++ src/views/data-information/Licensing-Result/index.tsx
@@ -1,4 +1,5 @@
 import {
+  CircularProgress,
   Grid,
   Paper,
   Table,
@@ -6,11 +7,80 @@ import {
   TableCell,
   TableContainer,
   TableHead,
+  TablePagination,
   TableRow,
   Typography
 } from '@mui/material'
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
+import { formatDate } from 'src/@core/components/formater';
+import ShowFilePDF from 'src/@core/components/show-file-pdf';
+import { getData } from 'src/api/axios';
 
 const LicenseResult = () => {
+
+  const [paramsFilter] = useState({
+    so_gp: null,
+    cong_trinh: 0,
+    coquan_cp: null,
+    loaihinh_cp: 0,
+    hieuluc_gp: null,
+    loai_ct: 0,
+    tang_chuanuoc: 0,
+    huyen: 0,
+    xa: 0,
+    tieuvung_qh: 0,
+    tochuc_canhan: 0,
+    tu_nam: 0,
+    den_nam: 0,
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [resData, setResData] = useState([]);
+
+  const isMounted = useRef(true);
+
+  const getDataLicense = async () => {
+    setLoading(true);
+    getData('giay-phep/danh-sach', paramsFilter)
+      .then((data) => {
+        if (isMounted.current) {
+          setResData(data);
+
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    isMounted.current = true
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    getDataLicense();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const [page, setPage] = useState<number>(0)
+  const [rowsPerPage, setRowsPerPage] = useState<number>(50)
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value)
+    setPage(0)
+  }
+
   return (
     <Grid className='_text_center'>
       <Typography className='font-weight-bold' sx={{ mt: 3 }} variant='h6'>
@@ -24,55 +94,57 @@ const LicenseResult = () => {
                 STT
               </TableCell>
               <TableCell size='small' align='center'>
-                Số giấy phép KTSDN
+                Số giấy phép
               </TableCell>
               <TableCell size='small' align='center'>
-                Tên giấy phép KTSDN
+                Tên giấy phép
               </TableCell>
               <TableCell size='small' align='center'>
-                Ngày ký giấy phép KTSDN
+                Ngày ký giấy phép
               </TableCell>
               <TableCell size='small' align='center'>
-                Ngày giấy phép có hiệu lực KTSDN
+                Thời hạn cấp phép
               </TableCell>
               <TableCell size='small' align='center'>
-                Ngày giấy phép hết hạn KTSDN
+                Cơ quan cấp phép
               </TableCell>
               <TableCell size='small' align='center'>
-                Cơ quan cấp phép KTSDN
+                Tên chủ giấy phép
               </TableCell>
               <TableCell size='small' align='center'>
-                Tên chủ giấy phép KTSDN
-              </TableCell>
-              <TableCell size='small' align='center'>
-                Địa chỉ chủ giấy phép KTSDN
-              </TableCell>
-              <TableCell size='small' align='center'>
-                File scan giấy phép KTSDN
-              </TableCell>
-              <TableCell size='small' align='center'>
-                File scan giấy tờ liên quan của giấy phép KTSDN
+                Địa chỉ chủ giấy phép
               </TableCell>
             </TableRow>
           </TableHead>
 
           <TableBody className='tableBody'>
-            <TableRow>
-              <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-              <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-              <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-              <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-              <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-              <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-              <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-              <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-              <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-              <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-              <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-              <TableCell className="text-center  size='small' align-middle font-13">-</TableCell>
-            </TableRow>
+
+            {loading ? <CircularProgress /> : resData?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((e: any, i) => {
+              return (
+                <TableRow key={i}>
+                  <TableCell className="text-center  size='small' align-middle font-13">{(page * rowsPerPage + i + 1)}</TableCell>
+                  <TableCell className="text-center  size='small' align-middle font-13">{<ShowFilePDF name={e.soGP} src={e.fileGiayPhep} />}</TableCell>
+                  <TableCell className="text-center  size='small' align-middle font-13">{e.tenGP}</TableCell>
+                  <TableCell className="text-center  size='small' align-middle font-13">{formatDate(e.ngayKy)}</TableCell>
+                  <TableCell className="text-center  size='small' align-middle font-13">{e.thoiHan}</TableCell>
+                  <TableCell className="text-center  size='small' align-middle font-13">{e.coQuanCapPhep}</TableCell>
+                  <TableCell className="text-center  size='small' align-middle font-13">{e.tochuc_canhan?.tenTCCN}</TableCell>
+                  <TableCell className="text-center  size='small' align-middle font-13">{e.tochuc_canhan?.diaChi}</TableCell>
+                </TableRow>
+              )
+            })}
+
           </TableBody>
         </Table>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, resData?.length]}
+          component='div'
+          count={resData?.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
       </TableContainer>
     </Grid>
   )
